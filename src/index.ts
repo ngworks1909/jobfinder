ad9c1bcf438bec8218987ea5ab9464c7e36e6f3f
ngworks1909 <nithin.kanduru1908@gmail.com>
2025-05-29 00:45:03 +0530

    v1

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 
 app.get('/', (_, res) => {
-    res.send('Hello World!')
+    res.send('Hello Nithin!')
 });
 app.listen(3000, () => console.log('Server running on port 3000'));
