5423f691d5245fb0d192e4239191af1b5ae743ed
ngworks1909 <nithin.kanduru1908@gmail.com>
2025-05-29 00:44:00 +0530

    v1

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const jobscrapper_1 = require("./scrappers/jobscrapper");
 const express_1 = __importDefault(require("express"));
-// Replace with your target URL
-const url = 'https://www.linkedin.com/jobs/search?keywords=Fullstack&location=500063&geoId=104508378&distance=25&f_TPR=r604800&f_PP=105556991&position=1&pageNum=0';
-(0, jobscrapper_1.scrapeJobs)(url);
 const app = (0, express_1.default)();
 app.get('/', (_, res) => {
     res.send('Hello World!');
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import { scrapeJobs } from "./scrappers/jobscrapper";
-import express from 'express'
-
-
-// Replace with your target URL
-const url = 'https://www.linkedin.com/jobs/search?keywords=Fullstack&location=500063&geoId=104508378&distance=25&f_TPR=r604800&f_PP=105556991&position=1&pageNum=0';
-
-scrapeJobs(url);
+import express from 'express';
 
 const app = express();
 
